2c5fca415ee55c30b894c428a9b56793f6e35c8c
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-08-24 08:26:14 +0900

    記事一覧取得APIの呼び出し・表示

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import type { Post } from '@prisma/client'
 import Link from 'next/link'
 import React from 'react'
 
-const Home = () => {
+const fetchAllBlogs = async (): Promise<Post[]> => {
+  const response = await fetch(
+    `${process.env.API_BASE_URL_LOCAL as string}/blog`,
+    { cache: 'no-store' },
+  )
+  const res = await response.json()
+
+  return res.posts
+}
+
+const Home = async () => {
+  const posts = await fetchAllBlogs()
+
   return (
     <main className="w-full h-full">
       <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-blue-900 drop-shadow-xl">
@@ -18,31 +31,40 @@ const Home = () => {
         </Link>
       </div>
 
-      <div className="w-full flex flex-col justify-center items-center">
-        <div className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-300 flex flex-col justify-center">
-          <div className="flex items-center my-3">
-            <div className="mr-auto">
-              <h2 className="mr-auto font-semibold">First Post</h2>
+      {posts.map((post: Post) => (
+        <div
+          key={post.id}
+          className="w-full flex flex-col justify-center items-center"
+        >
+          <div className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-300 flex flex-col justify-center">
+            <div className="flex items-center my-3">
+              <div className="mr-auto">
+                <h2 className="mr-auto font-semibold">{post.title}</h2>
+              </div>
+              <Link
+                href={`/blog/edit/${post.id}`}
+                className="px-4 py-1 text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200"
+              >
+                編集
+              </Link>
             </div>
-            <Link
-              href={`/blog/edit/1`}
-              className="px-4 py-1 text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200"
-            >
-              編集
-            </Link>
-          </div>
 
-          <div className="mr-auto my-1">
-            <blockquote className="font-bold text-slate-700">
-              2023/8/17
-            </blockquote>
-          </div>
+            <div className="mr-auto my-1">
+              <blockquote className="font-bold text-slate-700">
+                {new Date(post.cratedAt).toLocaleDateString('ja-JP', {
+                  year: 'numeric',
+                  month: '2-digit',
+                  day: '2-digit',
+                })}
+              </blockquote>
+            </div>
 
-          <div className="mr-auto my-1">
-            <h2>First Post Description</h2>
+            <div className="mr-auto my-1">
+              <h2>{post.description}</h2>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </main>
   )
 }
