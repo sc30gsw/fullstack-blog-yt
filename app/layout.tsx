589170987510af05c699106be5f8a2c818ae8327
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-08-24 08:37:49 +0900

    共通レイアウト化

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,7 +19,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-blue-900 drop-shadow-xl">
+          <h1 className="text-slate-200 text-center text-2xl font-extrabold">
+            <Link href="/" className="cursor-pointer">
+              Full Stack Blog 📝
+            </Link>
+          </h1>
+        </div>
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -17,20 +17,14 @@ const Home = async () => {
 
   return (
     <main className="w-full h-full">
-      <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-blue-900 drop-shadow-xl">
-        <h1 className="text-slate-200 text-center text-2xl font-extrabold">
-          Full Stack Blog 📝
-        </h1>
-      </div>
       <div className="flex my-5">
         <Link
           href={'/blog/add'}
-          className=" md:w-1/6 sm:w-2/4 text-center rounded-md p-2 m-auto bg-slate-300 font-semibold"
+          className=" md:w-1/6 sm:w-2/4 text-center rounded-md p-2 m-auto bg-slate-300 font-semibold hover:bg-gray-400 transition-all duration-300"
         >
           ブログ新規作成
         </Link>
       </div>
-
       {posts.map((post: Post) => (
         <div
           key={post.id}
