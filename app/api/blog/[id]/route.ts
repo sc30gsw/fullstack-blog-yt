d386a8316591daa105824f5979a9be8ba32b4151
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-08-24 07:33:47 +0900

    記事更新用APIの作成

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -5,12 +5,12 @@ import { main } from '../route'
 
 const prisma = new PrismaClient()
 
-// 記事詳細の取得
+// 記事詳細取得API
 export const GET = async (req: Request, res: NextResponse) => {
   try {
-    await main()
-
     const id = parseInt(req.url.split('/blog/')[1])
+
+    await main()
 
     const post = await prisma.post.findUnique({ where: { id } })
 
@@ -20,6 +20,35 @@ export const GET = async (req: Request, res: NextResponse) => {
     return NextResponse.json(
       { message: 'Succeeded in getting the post', post },
       { status: 200 },
+    )
+  } catch (err: any) {
+    return NextResponse.json({ message: err.message }, { status: 500 })
+  } finally {
+    await prisma.$disconnect()
+  }
+}
+
+// 記事更新用API
+export const PUT = async (req: Request, res: NextResponse) => {
+  try {
+    const id = parseInt(req.url.split('/blog/')[1])
+    const { title, description } = await req.json()
+
+    await main()
+
+    const post = await prisma.post.findUnique({ where: { id } })
+
+    if (!post)
+      return NextResponse.json({ message: 'Not Found post' }, { status: 404 })
+
+    const updatedPost = await prisma.post.update({
+      data: { title, description },
+      where: { id },
+    })
+
+    return NextResponse.json(
+      { message: 'Updated post successfully', updatedPost },
+      { status: 201 },
     )
   } catch (err: any) {
     return NextResponse.json({ message: err.message }, { status: 500 })
